a1a8d674e8adb9a3f6bf9bdc30b149b5945ebb94
VRamazing <r.vignesh149@gmail.com>
2017-09-26 22:08:53 +0530

    Score added.Reinitialize size on self bite

--- js/index.js
+++ js/index.js
@@ -1,183 +1,182 @@
 var canvas;
-var context;
-var trackBody;
-var lengthOfSnake;
-const BLOCK_SIZE = 20; //Size of each increment of snake part 
-var keyLog;
+var ctx;
+var xv = 50 ,yv = 50;
+var foodX,foodY;
+const BODY_SIZE = 23;
+const FOOD_SIZE = 10;
+var trail = [{ x : xv + BODY_SIZE , y : yv },
+             { x : xv + BODY_SIZE* 2   , y : yv },
+             { x : xv + BODY_SIZE * 3  , y : yv } ];
+
+//Length of trail excluding head
+const INITIAL_LENGTH = 3;
+var score = 0;
+
 window.onload = () => {
   canvas = document.getElementById("mainGame");
-  context = canvas.getContext('2d'); 
-  trackBody =  [
-                {"x" : canvas.width/2, "y" : canvas.height/2},
-                {"x" : canvas.width/2 + 20, "y" : canvas.height/2},
-                {"x" : canvas.width/2 + 40, "y" : canvas.height/2},
-                {"x" : canvas.width/2 + 60, "y" : canvas.height/2},
-                {"x" : canvas.width/2 + 80, "y" : canvas.height/2},
-                {"x" : canvas.width/2 + 80, "y" : canvas.height/2-20}
-  ];
-  lengthOfSnake = 6;
+  ctx = canvas.getContext('2d'); 
+  ctx.fillStyle = "white";
+  ctx.fillRect(0,0,canvas.width,canvas.height);
   
-  context.fillStyle = "white";
-  context.fillRect(0,0,canvas.width,canvas.height);
-  context.fillStyle = "black";
-  context.font="20px Arial";
-  context.fillText("Current Length : " + lengthOfSnake.toString() ,550,30);
-  // if i take each block to be 20 x 20 that would give we 40 blocks horizontal and 30 blocks vertically
-  context.fillStyle = "green";
-  document.onkeydown = function(evt){
-    switch(evt.key){
- 			case "ArrowRight":
-        keyLog = "right";
- 				break;
-      
-      case "ArrowLeft":
-        keyLog = "left";
- 			  break;
-    
- 			case "ArrowDown":
- 				keyLog = "down";
- 				break;
-    
- 			case "ArrowUp":
- 				keyLog = "up";
- 				break;
-    
- 		}
 
- 	}
-  
-  
-  trackBody.forEach((elem)=>{
-    plotBody(elem.x,elem.y);
-    
-  });
+  document.addEventListener('keydown',handleKeyDown);
+  var framesPerSecond = 100;
+  setInterval(mainGame,1000/framesPerSecond);
+  //Food
+  setInterval(dropFood,25 * framesPerSecond);
+
 };
 
-var framesPerSecond = 100;
-setInterval(() =>{moveEverything();drawEverything();},1000/framesPerSecond);
-  
 
-var moveEverything = () => {
-  if(keyLog == "up"){
-    //if currently moving left
-    //add element to the top of current index
-    var currentlyMoving = currentlyHeading();
-    var bodyElemPos = {"x":0,"y":0};
-    if(currentlyMoving == "left" || currentlyMoving == "right" || currentlyMoving == "up" ){
-      bodyElemPos.x = trackBody[0].x;
-      bodyElemPos.y = trackBody[0].y -  BLOCK_SIZE;
-      trackBody.unshift(bodyElemPos);
-      trackBody.pop();      
-    }
-    else if(currentlyMoving == "down"){
-      trackBody.reverse();     
-    }
-  }
+var mainGame = () => {
+
+ 
+  ctx.fillStyle = "white";
+  ctx.fillRect(0,0,canvas.width,canvas.height);
   
-   else if(keyLog == "down"){
-    //if currently moving left
-    //add element to the top of current index
-    var currentlyMoving = currentlyHeading();
-    var bodyElemPos = {"x":0,"y":0};
-    if(currentlyMoving == "left" || currentlyMoving == "right" || currentlyMoving == "down" ){
-      bodyElemPos.x = trackBody[0].x;
-      bodyElemPos.y = trackBody[0].y +  BLOCK_SIZE;
-      trackBody.unshift(bodyElemPos);
-      trackBody.pop();      
-    }
-    else if(currentlyMoving == "up"){
-      trackBody.reverse();     
-    }
+  //ball
+  if(!collision()){
+    ctx.fillStyle = "orange"
+    ctx.beginPath();
+    ctx.arc(foodX,foodY,FOOD_SIZE/2,0,Math.PI*2);
+    ctx.fill();
   }
+
+  else {    
+    
+    score++;
+    console.log(score);
+    //increasing length
+    for(var i = 0; i < 3; i++)
+      trail.unshift(trail[0]);
+
+    //displaying it outside the screen until reset.A way of deleting food4
   
-   else if(keyLog == "left"){
-    //if currently moving left
-    //add element to the top of current index
-    var currentlyMoving = currentlyHeading();
-    var bodyElemPos = {"x":0,"y":0};
-    if(currentlyMoving == "left" || currentlyMoving == "down" || currentlyMoving == "up" ){
-      bodyElemPos.x = trackBody[0].x -  BLOCK_SIZE;
-      bodyElemPos.y = trackBody[0].y;
-      trackBody.unshift(bodyElemPos);
-      trackBody.pop();      
-    }
-    else if(currentlyMoving == "right"){
-      trackBody.reverse();     
-    }
+    foodX = -100;
+    foodY = -100;
+    
   }
-  
-  else if(keyLog == "right"){
-    //if currently moving left
-    //add element to the top of current index
-    var currentlyMoving = currentlyHeading();
-    var bodyElemPos = {"x":0,"y":0};
-    if(currentlyMoving == "right" || currentlyMoving == "down" || currentlyMoving == "up" ){
-      bodyElemPos.x = trackBody[0].x +  BLOCK_SIZE;
-      bodyElemPos.y = trackBody[0].y;
-      trackBody.unshift(bodyElemPos);
-      trackBody.pop();      
-    }
-    else if(currentlyMoving == "left"){
-      trackBody.reverse();     
-    }
-  }
-  
+
+
+  checkBoundaries();
+
+  //drawing snake bouderies
+  ctx.fillStyle = "#bada55";
+  ctx.fillRect(xv,yv,BODY_SIZE - 2,BODY_SIZE - 2 );
+
+  checkSelfCollision();
+
+  //draw an array of rects
+  trail.forEach(function(elem,index){
+      ctx.fillRect(elem.x  ,elem.y,BODY_SIZE - 2,BODY_SIZE - 2)
+  })
+
+  ctx.fillStyle = "black";
+  ctx.font="20px Arial";
+  ctx.fillText("Score : " + score ,290,30);
+  console.log(score);
 }
 
+var checkSelfCollision = () => {
+  var temp  = {x : xv , y:yv};
+  //size reset
+  trail.forEach(function(elem){
 
-
-
-var currentlyHeading = () => {
-  //Compares first and second element to see where the head of snake is headed
-  
-  if(trackBody[0].y == trackBody[1].y){
-    if(trackBody[0].x < trackBody[1].x ){
-      //headed left
-      return "left";
-      // bodyElemPos.x = trackBody[0].x - BLOCK_SIZE;
-      // bodyElemPos.y = trackBody[0].y;
-      // trackBody.unshift(bodyElemPos);
-      // trackBody.pop();
-    }
-    else{
-      //headed right
-      // bodyElemPos.x = trackBody[0].x + BLOCK_SIZE;
-      // bodyElemPos.y = trackBody[0].y;
-      // trackBody.unshift(bodyElemPos);
-      // trackBody.pop();
-      return "right";
+     if (elem.x === temp.x && elem.y === temp.y){
       
-    }
-  }
-  
-  else if(trackBody[0].x == trackBody[1].x){
-    if(trackBody[0].y < trackBody[1].y ){
-      //headed top
-      // bodyElemPos.x = trackBody[0].x 
-      // bodyElemPos.y = trackBody[0].y - BLOCK_SIZE;;
-      // trackBody.unshift(bodyElemPos);
-      // trackBody.pop();
-      return "top";
-    }
-    else{
-      //headed bottom
-      // bodyElemPos.x = trackBody[0].x 
-      // bodyElemPos.y = trackBody[0].y + BLOCK_SIZE;;
-      // trackBody.unshift(bodyElemPos);
-      // trackBody.pop();
-      return "bottom";
-    }
-  
-  }
+      trail = trail.slice(0,INITIAL_LENGTH );
+      score = 0;
+      
+     } 
+  })
+  //score reset
   
 }
 
-var drawEverything = () => {
-   trackBody.forEach((elem)=>{
-    plotBody(elem.x,elem.y);
-  });
+
+
+var collision = () => {
+  var bodyBox={
+    x : xv - BODY_SIZE/2,
+    y : yv - BODY_SIZE/2,
+    width : BODY_SIZE,
+    height : BODY_SIZE
+
+  }
+  var foodBox={
+    x : foodX - FOOD_SIZE/2,
+    y : foodY - FOOD_SIZE/2,
+    width : FOOD_SIZE,
+    height : FOOD_SIZE
+
+  }
+
+  return testCollisionRect(bodyBox,foodBox);
 }
 
-var plotBody = (posX,posY) => {
-   context.fillRect(posX,posY,BLOCK_SIZE,BLOCK_SIZE);
+
+var  testCollisionRect = (rect1,rect2) => {
+  return rect1.x <= rect2.x + rect2.width
+    && rect2.x <= rect1.x + rect1.width
+    && rect1.y <= rect2.y + rect2.height
+    && rect2.y <= rect1.y + rect1.height;
+
+ }
+
+
+var dropFood = () => {
+  foodX = Math.floor(Math.random(0,1)*canvas.width);
+  foodY = Math.floor(Math.random(0,1)*canvas.height);
+}
+
+
+
+var checkBoundaries = () => {
+  if(xv < 0 ){
+      xv = canvas.width - BODY_SIZE
+  }
+  else if (xv > canvas.width){
+    xv = BODY_SIZE
+  }
+  else if(yv < 0 ){
+      yv = canvas.height - BODY_SIZE
+  }
+  else if (yv > canvas.height){
+    yv = BODY_SIZE
+  }
+
+}
+
+
+var handleKeyDown = (evt) => {
+    
+    switch(evt.key){
+      case "ArrowRight":
+        trail.pop();
+        trail.unshift({x : xv , y : yv });
+        xv += BODY_SIZE ; 
+        
+        break;
+      
+      case "ArrowLeft":
+        trail.unshift({x : xv , y : yv });
+        xv -= BODY_SIZE ;
+        trail.pop();
+        break;
+    
+      case "ArrowDown":
+        trail.pop();
+        trail.unshift({x : xv , y : yv });
+        yv += BODY_SIZE;
+        
+        break;
+    
+      case "ArrowUp":
+        trail.unshift({x : xv  , y : yv });
+        yv -= BODY_SIZE; 
+        trail.pop();
+        break;      
+    
+    }
+
 }
